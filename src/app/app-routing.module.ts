dd05ffd7e8fa67917483e108e078df20b7954be0
wassFCP <awassell@fcptech.com>
2018-05-21 10:42:11 +0100

    learning angular 6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HeroesComponent} from './heroes/heroes.component';
 
 const routes: Routes = [
+    {path: 'heroes', component: HeroesComponent}
 ];
 
 @NgModule({
